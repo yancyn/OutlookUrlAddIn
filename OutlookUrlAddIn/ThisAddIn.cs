0a72b355a7950155c5ce133dd51e8a5ed22e80eb
yancyn <yancyn@gmail.com>
2017-06-13 23:01:51 +0800

    Add sample to show active explorer

--- OutlookUrlAddIn/ThisAddIn.cs
+++ OutlookUrlAddIn/ThisAddIn.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Xml.Linq;
 using Outlook = Microsoft.Office.Interop.Outlook;
 using Office = Microsoft.Office.Core;
+using System.Windows.Forms;
 
 namespace OutlookUrlAddIn
 {
@@ -13,8 +14,24 @@ namespace OutlookUrlAddIn
         private Outlook.Inspectors inspectors;
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            Outlook.Application application = this.Application;
+
             this.inspectors = this.Application.Inspectors;
             this.inspectors.NewInspector += inspectors_NewInspector;
+
+            Outlook.Inspector activeInspector = application.ActiveInspector();
+            if (activeInspector != null)
+            {
+                MessageBox.Show("Active inspector: " + activeInspector.Caption);
+            }
+
+            // Get the Explorer objects
+            Outlook.Explorers explorers = application.Explorers;
+            Outlook.Explorer activeExplorer = application.ActiveExplorer();
+            if (activeExplorer != null)
+            {
+                MessageBox.Show("Active explorer: " + activeExplorer.Caption);
+            }
         }
 
         private void inspectors_NewInspector(Outlook.Inspector Inspector)
