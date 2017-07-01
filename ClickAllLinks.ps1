ff27450ffead1f807855a5da832b9d0d8a36270a
yancyn <yancyn@gmail.com>
2017-07-01 16:44:56 +0800

    Clean up auto submit script

--- ClickAllLinks.ps1
+++ ClickAllLinks.ps1
@@ -1,24 +1,23 @@
 param([Int32]$idle=1000)
 #idle time parameter from command prompt. idle time 1000ms = 1s
 
-Clear-Host
-
-# Parse from urls.txt
+# Wrap from url.txt into array.
+# Make sure source file url.txt must separate url by new line
 # $urls = @("http://news.google.com", "https://www.yahoo.com/news/")
 $urls = Get-Content url.txt
 
 $ie = New-Object -COMObject InternetExplorer.Application
-$ie.Visible = $True
+$ie.Visible = $True # set to false to run at background
 Add-Type -AssemblyName System.Windows.Forms
 
 # Start loop collection of url list and navigate
-Add-Type -AssemblyName PresentationFramework
-# For($i=0;$i -lt $urls.Length;$i++) {
-foreach($url in $urls) {
+foreach($url in $urls) { # For($i=0;$i -lt $urls.Length;$i++)
     Start-Sleep -s 1
     $ie.Navigate($url)
     [System.Windows.Forms.SendKeys]::SendWait("{TAB}");Start-Sleep -m $idle
     [System.Windows.Forms.SendKeys]::SendWait("{ENTER}");Start-Sleep -m $idle
 }
 $ie.Quit()
+
+Add-Type -AssemblyName PresentationFramework
 [System.Windows.MessageBox]::Show("Done", "Auto Submit")
