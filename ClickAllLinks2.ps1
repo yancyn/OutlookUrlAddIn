4db28cc1d21250ae19161917f8ed2947985c0b02
yancyn <yancyn@gmail.com>
2017-11-18 18:10:04 +0800

    HACK: Run second link twice due to always failed to execute in ClickAllLinks.ps1

---
+++ ClickAllLinks2.ps1
@@ -0,0 +1,44 @@
+param([Int32]$idle=500)
+#idle time parameter from command prompt. idle time 1000ms = 1s
+
+# Wrap from url.txt into array.
+# Make sure source file url.txt must separate url by new line
+# $urls = @("http://news.google.com", "https://www.yahoo.com/news/")
+$urls = Get-Content url.txt
+
+$ie = New-Object -COMObject InternetExplorer.Application
+$ie.Visible = $True # set to false to run at background
+Add-Type -AssemblyName System.Windows.Forms
+
+# Start loop collection of url list and navigate
+# foreach($url in $urls) {
+For($i=0;$i -lt $urls.Length;$i++) {
+    echo $url
+    $ie.Navigate($urls[$i])
+
+    # see http://www.westerndevs.com/simple-powershell-automation-browser-based-tasks/
+    while($ie.Busy) { Start-Sleep -Milliseconds 100 }
+
+    $doc = $ie.Document
+    $chk = $doc.getElementById("decisionForAll_1")
+    $chk.click()
+
+    $btn = $doc.getElementById("Approval2_Submit")
+    $btn.click()
+    Start-Sleep -s 2
+
+    // HACK: Not sure why always failed at second url so repeat
+    if($i==1) {
+        while($ie.Busy) { Start-Sleep -Milliseconds 100 }
+        $doc = $ie.Document
+        $chk = $doc.getElementById("decisionForAll_1")
+        $chk.click()
+        $btn = $doc.getElementById("Approval2_Submit")
+        $btn.click()
+        Start-Sleep -s 2
+    }
+}
+$ie.Quit()
+
+Add-Type -AssemblyName PresentationFramework
+[System.Windows.MessageBox]::Show("Done", "Auto Submit")
